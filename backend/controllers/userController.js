ee6007c567c4ae9a9642c186eb568b5fe28455c2
KanujanS <kanujansk@gmail.com>
2025-03-07 16:40:13 +0530

    login & register backend code update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,8 +19,10 @@ try {
         return res.json({success:false,message:"Invalid credentials"})
     }
     const token = createToken(user._id);
+    res.json({success:true,token})
 } catch (error) {
-    
+    console.log(error);
+    res.json({success:false,message:"Error"})
 }
 }
 
